f15619bfb641cf499c78a93f52297d1514086fa1
Kalinin Kirill <76593316+sw88tch@users.noreply.github.com>
2023-09-08 07:32:38 -0400

    Add files via upload
    old version

---
+++ src/helpers.js
@@ -0,0 +1,26 @@
+export const shuffleAnswers = question => {
+    const unshuffledAnswers = [
+        question.correctAnswer,
+        ...question.incorrectAnswers
+    ]
+
+    return unshuffledAnswers.map(unshuffledAnswers => ({
+        sort: Math.random(),
+        value: unshuffledAnswers
+    }))
+        .sort((a, b) => a.sort - b.sort)
+        .map((a) => a.value);
+}
+
+export const normalizeQuestions = backendQuestions => {
+    return backendQuestions.map(backendQuestion => {
+        const incorrectAnswers = backendQuestion.incorrect_answers.map(
+            incorrectAnswers => decodeURIComponent(incorrectAnswers)
+        );
+        return {
+            correctAnswer: decodeURIComponent(backendQuestion.correct_answer),
+            question: decodeURIComponent(backendQuestion.question),
+            incorrectAnswers
+        }
+    });
+}
